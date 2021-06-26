d95324220d8fbf54b297475c23e62f3b4ffb296b
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2021-06-26 18:21:51 +0500

    delete Manager class. Add Cart class

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
 
-use app\models\{Manager, Model, Order, Product, User};
+use app\models\{Model, Product, User, Cart, Order};
+use app\engine\Autoload;
 
 include "../engine/Autoload.php";
 spl_autoload_register([new Autoload, "loadClass"]);
 
 $user = new User;
+var_dump($user);
+
+$product = new Product;
 var_dump($user);
