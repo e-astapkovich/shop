99eac2939a5a2deba0faf7ce8aebd7a595c78772
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2021-06-28 00:32:35 +0500

    Готово по третьей лекции, перед ДЗ

--- public/index.php
+++ public/index.php
@@ -6,13 +6,17 @@ use app\engine\{Autoload, Db};
 include "../engine/Autoload.php";
 spl_autoload_register([new Autoload, "loadClass"]);
 
+
 $user = new User();
 var_dump($user->getOne(6)->password_hash);
 var_dump($user->getOne(4)->id);
 var_dump($user->getOne(4));
-// var_dump($user->getAll());
+var_dump($user->getAll());
 
-// $product = new Product();
-// var_dump($product->getOne(5));
-// echo $product->getOne(5);
-// echo $product->getAll();
+
+// public $id;
+// public $name_product;
+// public $price;
+// public $image_name;
+// public $category_id;
+// public $manufacturer_id;
