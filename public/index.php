8676cb7aa2bce39df43bedc2779cdd909758a89c
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2021-06-27 19:35:31 +0500

    small correction

--- public/index.php
+++ public/index.php
@@ -6,9 +6,6 @@ use app\engine\{Autoload, Db};
 include "../engine/Autoload.php";
 spl_autoload_register([new Autoload, "loadClass"]);
 
-// $db = new Db;
-// $db->getConnection();
-
 $user = new User();
 var_dump($user->getOne(6));
 var_dump($user->getAll());
