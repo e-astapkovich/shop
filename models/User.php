792258cd6c53a2bcc8491bc6c2e3954ba9802475
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2021-07-04 14:06:19 +0500

    Добавил констукторы во все имеющиеся модели. Значение по умолчанию исправил с пустой строки на null

--- models/User.php
+++ models/User.php
@@ -10,6 +10,15 @@ class User extends Model {
     public $status;
     public $manager_id;
 
+    public function __construct($name_user = null, $password_hash = null, $role = null, $status = null, $manager_id = null)
+    {
+        $this->name_user = $name_user;
+        $this->password_hash = $password_hash;
+        $this->role = $role;
+        $this->status = $status;
+        $this->manager_id = $manager_id;
+    }
+
     protected function getTableName() {
         return 'users';
     }
